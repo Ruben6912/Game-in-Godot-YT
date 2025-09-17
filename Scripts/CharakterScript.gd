83bce1de8b8f5443fe9e57b6f62db2337d470ec1
Ruben Leuzzi <ruben.leuzzi@gmx.de>
2025-09-17 12:14:01 +0200

    added a character and made some small adjustments
    Added the Entity class
    Added the Player itself with a movement script implementing the Entity class
    Added some folders
    Made some adjustments for future development

---
+++ Scripts/CharakterScript.gd
@@ -0,0 +1,46 @@
+extends "res://Scripts/Entity.gd"
+class_name Player
+
+@onready var player_anim: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	var input_vector = Vector2.ZERO
+	
+	# Input
+	if Input.is_action_pressed("move_right"):
+		input_vector.x += 1
+	if Input.is_action_pressed("move_left"):
+		input_vector.x -= 1
+	if Input.is_action_pressed("move_down"):
+		input_vector.y += 1
+	if Input.is_action_pressed("move_up"):
+		input_vector.y -= 1
+	
+	# Normalize and apply speed
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector.normalized() * speed
+	else:
+		velocity = Vector2.ZERO
+	
+	# Move with collision
+	move_and_slide()
+	
+	# Flip sprite if moving left/right
+	if velocity.x != 0:
+		player_anim.flip_h = velocity.x < 0
+	
+	# Play correct animation
+	if velocity.length() > 0:
+		_play_walk_animation()
+	else:
+		_play_idle_animation()
+	print(velocity)
+
+
+func _play_walk_animation() -> void:
+	if player_anim.animation != "Run":
+		player_anim.play("Run")
+
+func _play_idle_animation() -> void:
+	if player_anim.animation != "Idle":
+		player_anim.play("Idle")
--- Scripts/Entity.gd
+++ Scripts/Entity.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
+class_name Entity
 
-@export var max_health: int = 100
+@export var max_health: int = 3
 @export var health: int = max_health
 @export var defense: int = 0
+@export var speed: float 
