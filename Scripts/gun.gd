079a3e248c300ac36044d10d06c3ac0707ef9f8c
Ruben Leuzzi <ruben.leuzzi@gmx.de>
2025-09-17 22:30:23 +0200

    gun and bullet
    Gun and Bullet scenes added

---
+++ Scripts/gun.gd
@@ -0,0 +1,9 @@
+extends Node
+var bullet_lifetime: float
+var bullet_speed: float
+var reload_time: float
+var damage: float
+var bullet_per_shot: int 
+var fire_rate: float
+var spread_angle: float
+var is_hitscan: bool
